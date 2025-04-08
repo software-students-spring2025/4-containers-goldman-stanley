03feaac6e7f0c61a35ae82bbc67d5795e24f9027
manrongm <mm13721@nyu.edu>
2025-04-08 17:03:01 -0400

    updated style

--- web-app/app.py
+++ web-app/app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request
-import pymongo
+from flask import Flask, render_template, request, jsonify
 from datetime import datetime
+import pymongo
 import os
+import requests
+
+mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+ML_CLIENT_HOST = os.getenv("ML_CLIENT_HOST", "http://localhost:6000")
 
 app = Flask(__name__)
 
-mongo_uri = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
 client = pymongo.MongoClient(mongo_uri)
 db = client["sound_analysis"]
 
@@ -18,6 +21,18 @@ def index():
         e['timestamp'] = e['timestamp'].strftime('%Y-%m-%d %H:%M:%S')
     return render_template('index.html', events=events, filter=filter_type)
 
+@app.route('/upload-audio', methods=['POST'])
+def upload_audio():
+    audio = request.files['audio']
+
+    try:
+        response = requests.post(f"{ML_CLIENT_HOST}/analyze", files={'audio': audio})
+        return jsonify(response.json()), response.status_code
+    except ValueError:
+        return jsonify({"error": "ML Client did not return valid JSON", "raw_response": response.text}), 500
+    except Exception as e:
+        return jsonify({"error": f"Failed to connect to ML client: {str(e)}"}), 500
+
 if __name__ == '__main__':
-    print("Starting Flask app on 0.0.0.0:5000...")
     app.run(host='0.0.0.0', port=5050)
+
